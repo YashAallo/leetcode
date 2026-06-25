2a46aa0202024da6703046c4fe247a96b74f5d04
Yash Raj <yashrajsingh003@gmail.com>
2026-06-24 21:51:18 -0700

    Time: 50 ms (40.09%), Space: 39.7 MB (57.52%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -4,24 +4,16 @@ public:
 
         int n = nums.size();
 
-        vector<int> v;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == target)
-                v.push_back(i);
-        }
-
         int ans = 0;
+
         for (int l = 0; l < n; l++) {
+            
+            int count = 0;
             for (int r = l; r < n; r++) {
-                
 
-               
+                if (nums[r] == target)
+                    count++;
 
-                auto low = lower_bound(v.begin(), v.end(), l);
-                auto up = upper_bound(v.begin(), v.end(), r);
-
-                int count = up - low;
                 int len = r - l + 1;
 
                 if (2 * count > len)
