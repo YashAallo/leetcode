cda05a9f10d496a3c5f2874bf259be16b54bec5f
Yash Raj <yashrajsingh003@gmail.com>
2026-06-30 00:03:07 -0700

    Time: 0 ms (100%), Space: 18.1 MB (6.04%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-    int bs(vector<int>& nums, int target,int l,int r) {
-        
-       
-        while (l <= r) {
-
-            int mid = l + (r - l) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return -1;
-    }
-
     bool search(vector<int>& nums, int target) {
 
         int n = nums.size();
@@ -25,58 +7,50 @@ public:
         int l = 0;
         int r = n - 1;
 
-        while (l < r) {
+        while (l <= r) {
             int mid = l + (r - l) / 2;
 
-            if (nums[mid] > nums[r]) {
-                l = mid + 1;
-            } else if (nums[mid] < nums[r]) {
-                r = mid;
-            } else if(nums[mid]==nums[l] && nums[r]==nums[mid]) {
-             
-                while(l<r)
-                {
-                    l++;
-                    r--;
+            if (nums[mid] == target)
+                return true;
 
-                    if(nums[l]>nums[mid] )
-                    {
-                        l++;
-                        break;
-                    }
-                    else if( nums[r]>nums[mid])
-                    {
-                        l=r+1;
-                    }
-                    if(nums[l]<nums[mid] )
-                    {
-                        
-                        break;
-                    }
-                    else if( nums[r]<nums[mid])
-                    {
-                        l=r;
-                    }
-                }
-            } else
-            {
+            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                l++;
                 r--;
+            } else if (nums[l] <= nums[mid]) {
+                // left half is sorted
+
+                if(target<= nums[mid] && target >= nums[l])
+                {
+                    r=mid;
+                }
+                else
+                {
+                    l=mid+1;
+                }
+
+
+            } else {
+                // right half is sorted
+
+                if(target >=nums[mid] && target <= nums[r])
+                {
+                    l=mid;
+                }
+                else 
+                {
+                    r=mid-1;
+                }
+
+
             }
         }
 
-        int pivot=l;
+        cout << l;
 
-        cout<<l<<" ";
+        //edge case when l goes outside array
+        if(l==n)
+        return false;
 
-        int ans = bs(nums, target, 0, pivot - 1);
-
-        cout<<ans<<" ";
-
-        if (ans == -1)
-            ans = bs(nums, target, pivot, n - 1);
-
-               cout<<ans<<" ";
-
-        return ans>-1;
+        return nums[l] == target;
     }
 };
