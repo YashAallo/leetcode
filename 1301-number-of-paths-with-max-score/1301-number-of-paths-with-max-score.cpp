f8ca402e8b86db19c59d9050213cbf90268f0a3b
Yash Raj <yashrajsingh003@gmail.com>
2026-07-05 09:58:44 -0700

    Time: 6 ms (82.23%), Space: 11.6 MB (58.19%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -1,81 +1,66 @@
 class Solution {
-    int row[3] = {0, -1, -1}; // 0,-1  -1,0  -1,-1
-    int col[3] = {-1, 0, -1}; // 0,-1  -1,0  -1,-1
+    const int MOD = 1e9 + 7;
 
-    int func(vector<string>& board, int i, int j, vector<vector<int>>& dp) {
+    int row[3] = {0, -1, -1};
+    int col[3] = {-1, 0, -1};
+
+    pair<int, int> func(vector<string>& board, int i, int j,
+                        vector<vector<pair<int, int>>>& dp) {
+
         if (i == 0 && j == 0)
-            return 0;
+            return {0, 1};
 
-        if (dp[i][j] != -1)
+        if (dp[i][j] != make_pair(-1, -1))
             return dp[i][j];
 
         int maxi = INT_MIN;
+        int ways = 0;
+
         for (int k = 0; k < 3; k++) {
+
             int r = i + row[k];
             int c = j + col[k];
 
-            if (r >= 0 && c >= 0 && board[r][c] != 'X') {
-                int num = 0;
+            if (r < 0 || c < 0 || board[r][c] == 'X')
+                continue;
 
-                if (r != 0 || c != 0)
-                    num = board[r][c] - '0';
+            auto [prevScore, prevWays] = func(board, r, c, dp);
 
-                maxi = max(maxi, num + func(board, r, c, dp));
+            if (prevScore == INT_MIN)
+                continue;
+
+            int num = 0;
+            if (isdigit(board[r][c]))
+                num = board[r][c] - '0';
+
+            int score = prevScore + num;
+
+            if (score > maxi) {
+                maxi = score;
+                ways = prevWays;
+            }
+            else if (score == maxi) {
+                ways = (ways + prevWays) % MOD;
             }
         }
 
-        long long mod = 1e9 + 7;
-        return dp[i][j] = maxi % mod;
-    }
-
-    int path(vector<string>& board, int i, int j, int target,
-             vector<vector<vector<int>>>& dp) {
-
-        if (target < 0)
-            return 0;
-
-        if (i == 0 && j == 0) {
-            if (target == 0)
-                return 1;
-        }
-
-        if (dp[i][j][target] != -1)
-            return dp[i][j][target];
-
-        int sum = 0;
-        for (int k = 0; k < 3; k++) {
-            int r = i + row[k];
-            int c = j + col[k];
-
-            if (r >= 0 && c >= 0 && board[r][c] != 'X') {
-                int num = 0;
-
-                if (r != 0 || c != 0)
-                    num = board[r][c] - '0';
-
-                sum += path(board, r, c, target - num, dp);
-            }
-        }
-
-        return dp[i][j][target] = sum;
+        return dp[i][j] = {maxi, ways};
     }
 
 public:
     vector<int> pathsWithMaxScore(vector<string>& board) {
 
         int n = board.size();
-        int m = board[0].size();
 
-        vector<vector<int>> dp1(n + 1, vector<int>(m + 1, -1));
+        vector<vector<pair<int, int>>> dp(
+            n,
+            vector<pair<int, int>>(n, {-1, -1}));
 
-        int maxi = func(board, n - 1, m - 1, dp1);
-        maxi = max(maxi, 0);
+        auto [score, ways] = func(board, n - 1, n - 1, dp);
 
-        vector<vector<vector<int>>> dp2(
-            n, vector<vector<int>>(m, vector<int>(maxi + 1, -1)));
+        if (score == INT_MIN)
+            return {0, 0};
 
-        int validPath = path(board, n - 1, m - 1, maxi, dp2);
-
-        return {maxi, validPath};
+        return {score, ways};
     }
 };
