df96613c596cf35b2d65ba1a488bc5528b60c132
Yash Raj <yashrajsingh003@gmail.com>
2026-07-05 20:52:03 -0700

    Time: 74 ms (6.49%), Space: 33.7 MB (5.3%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
 
-        int n=intervals.size();
+        int n = intervals.size();
 
         sort(intervals.begin(), intervals.end(), [](auto a, auto b) {
             if (a[0] == b[0])
@@ -11,21 +11,16 @@ public:
             return a[0] < b[0];
         });
 
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        int count = 0;
-        for (auto interval : intervals) {
+        int ans = 0;
+        int maxEnd = -1;
 
-            if (start <= interval[0] && end >= interval[1]) {
-                count++;
-            } else if (end <= interval[0]) {
-                start = interval[0];
-                end = interval[1];
-            } else {
-
-                end = interval[1];
+        for (const auto& interval : intervals) {
+            if (interval[1] > maxEnd) {
+                ans++;
+                maxEnd = interval[1];
             }
         }
-        return n-(count-1);
+
+        return ans;
     }
 };
